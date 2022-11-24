c7b3cde20e6f8f0c7d90119a8d152c164bde0801
murat <celiktepemurat@gmail.com>
2022-11-24 16:18:34 +0300

    feat: write player metrics

--- scripts/writeScripts/writePastData.ts
+++ scripts/writeScripts/writePastData.ts
@@ -8,12 +8,12 @@ dotenv.config();
 const PROVIDER = ethers.providers.getDefaultProvider('http://localhost:8545');
 const SIGNER = new ethers.Wallet(process.env.PRIV_KEY as string, PROVIDER);
 
-const PROXY_STAT = '0x1e011a7Fe24AA92b2623eB6efEE51E640A109236';
+const PROXY_STAT = '0xf19e93c0B2B43e69b9b95F833161580480882a42';
 const statistics = new ethers.Contract(PROXY_STAT, STATISTICS_ABI, SIGNER);
 
 const main = async () => {
-  //await savePlayers();
-  await saveGlobalNumbers();
+  await savePlayers();
+  //await saveGlobalNumbers();
 };
 
 const saveGlobalNumbers = async () => {
@@ -35,7 +35,7 @@ const savePlayers = async () => {
   const ALL_PLAYERS_PATH = `./data/all_player_list.json`;
   const players = loadFetchedData(ALL_PLAYERS_PATH).players;
 
-  const txn = await statistics.savePlayers(players.slice(0, 500));
+  const txn = await statistics.updatePlayers(players.slice(0, 500));
   console.log(await txn.wait());
   let receivedTxn = await txn.wait();
   console.log('Gas Used -> ', receivedTxn.gasUsed.toString());
